a3910b727be15f6644780eb41c86eb12750bb739
Bijay Sharma <digitalbijay17@gmail.com>
2026-06-30 19:37:44 +0530

    Sidebar collapsable

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import SideBarNavigation from "../components/SidebarNavigations";
 
 const RootLayout = () => {
-  const [sidebarActive, setSidebarActive] = useState(false);
+  const [sidebarActive, setSidebarActive] = useState(true);
 
   const handleSidebarToggle = () => {
     setSidebarActive((prevState) => !prevState);
