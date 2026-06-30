2797358a7bdfd2f875cba4c250923f5fbc934ea8
1bijaysharma <bijay.sharma@creoate.com>
2026-06-30 14:37:26 +0530

    added env variable debug log

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -12,3 +12,8 @@ const {
   data: { session },
 } = await supabase.auth.getSession();
 console.log("session:", session);
+
+console.log("MODE:", import.meta.env.MODE);
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+console.log(import.meta.env);
